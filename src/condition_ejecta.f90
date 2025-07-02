d3cea07114d0debe0d63f0cb8d0bb17dd1976982
Sho Fujibayashi <sfujibayashi@yamazaki-06.aei.mpg.de>
2025-07-02 14:42:08 +0200

    change criterion

--- src/condition_ejecta.f90
+++ src/condition_ejecta.f90
@@ -17,7 +17,8 @@ logical function condition_ejecta(j,k,l,lv,rfl,rin,hhh_crit)
   !    condition_ejecta=.true.
   ! endif
 
-  condition_ejecta = condition_ejecta_geo(j,k,l,lv,rfl,rin)
+  ! condition_ejecta = condition_ejecta_geo(j,k,l,lv,rfl,rin)
+  condition_ejecta = condition_ejecta_bernoulli(j,k,l,lv,rfl,rin)
 
   return
 end function condition_ejecta
@@ -34,7 +35,7 @@ logical function condition_ejecta_bernoulli(j,k,l,lv,rfl,rin,hhh_crit)
   condition_ejecta_bernoulli = .false.
 
   if(-ut(j,k,l,lv)*hhh(j,k,l,lv)-hhh_crit> 0.d0  &
-       .and. vlx(j,k,l,lv)*x(j,lv) + vly(j,k,l,lv)*y(k,lv) + vlz(j,k,l,lv)*z(l,lv) > 0.d0 &
+       ! .and. vlx(j,k,l,lv)*x(j,lv) + vly(j,k,l,lv)*y(k,lv) + vlz(j,k,l,lv)*z(l,lv) > 0.d0 &
        .and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)<rfl &
        !.and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)>rin &
        )then
@@ -43,6 +44,27 @@ logical function condition_ejecta_bernoulli(j,k,l,lv,rfl,rin,hhh_crit)
 
   return
 end function condition_ejecta_bernoulli
+
+logical function condition_ejecta_bernoulli_positive_velocity(j,k,l,lv,rfl,rin,hhh_crit)
+  use simdata3D
+  use particle_data
+  
+  implicit none
+  integer,intent(in) :: j,k,l,lv
+  real(8),intent(in) :: rfl,rin,hhh_crit
+
+  condition_ejecta_bernoulli_positive_velocity = .false.
+
+  if(-ut(j,k,l,lv)*hhh(j,k,l,lv)-hhh_crit> 0.d0  &
+       .and. vlx(j,k,l,lv)*x(j,lv) + vly(j,k,l,lv)*y(k,lv) + vlz(j,k,l,lv)*z(l,lv) > 0.d0 &
+       .and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)<rfl &
+       !.and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)>rin &
+       )then
+     condition_ejecta_bernoulli_positive_velocity = .true.
+  endif
+
+  return
+end function condition_ejecta_bernoulli_positive_velocity
 
 logical function condition_ejecta_geo(j,k,l,lv,rfl,rin)
   use simdata3D
--- src/main.F90
+++ src/main.F90
@@ -170,8 +170,8 @@ program main
   write(*,'("result saved in : ",a)') trim(dir_out)
   
   ! call ascii(model,dir_out,it_skip_out)
-  call tr_analysis(model,dir_out)
-  stop
+  ! call tr_analysis(model,dir_out)
+  ! stop
 
   
   write(6,'("it_skip, it_skip_out      : ",2i5)') it_skip,it_skip_out
