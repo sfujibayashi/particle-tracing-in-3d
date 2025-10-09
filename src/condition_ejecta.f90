0a4e2c497673ede9c602d7f73ae27108d33421d8
Sho Fujibayashi <shofu@sakura01.bc.rzg.mpg.de>
2025-10-09 16:19:15 +0200

    hu_t+1<0 condition added

--- src/condition_ejecta.f90
+++ src/condition_ejecta.f90
@@ -45,6 +45,29 @@ logical function condition_ejecta_bernoulli(j,k,l,lv,rfl,rin,hhh_crit)
   return
 end function condition_ejecta_bernoulli
 
+
+logical function condition_ejecta_hut1(j,k,l,lv,rfl,rin,hhh_crit)
+  use simdata3D
+  use particle_data
+  
+  implicit none
+  integer,intent(in) :: j,k,l,lv
+  real(8),intent(in) :: rfl,rin,hhh_crit
+
+  condition_ejecta_hut1 = .false.
+
+  if(-ut(j,k,l,lv)*hhh(j,k,l,lv)-1d0> 0.d0  &
+       ! .and. vlx(j,k,l,lv)*x(j,lv) + vly(j,k,l,lv)*y(k,lv) + vlz(j,k,l,lv)*z(l,lv) > 0.d0 &
+       .and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)<rfl &
+       .and. sqrt(x(j,lv)**2+y(k,lv)**2+z(l,lv)**2)>=rin &
+       )then
+     condition_ejecta_hut1 = .true.
+  endif
+
+  return
+end function condition_ejecta_hut1
+
+
 logical function condition_ejecta_bernoulli_positive_velocity(j,k,l,lv,rfl,rin,hhh_crit)
   use simdata3D
   use particle_data
